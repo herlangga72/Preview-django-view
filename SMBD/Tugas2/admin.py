34d7e7a377ab85f44a590f69b0b820e475ab8e93
L200180204 <yoghaap7@gmail.com>
2020-10-25 19:22:19 +0700

    change

--- SMBD/Tugas2/admin.py
+++ SMBD/Tugas2/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import produk, mengemas, karyawan
 # Register your models here.
+admin.site.register(mengemas)
+admin.site.register(produk)
+admin.site.register(karyawan)
+
