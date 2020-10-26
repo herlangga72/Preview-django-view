7bf8add30f3dd498cbe87f27f2576cddd652e4f5
herlangga72 <herlangga72@gmail.com>
2020-10-26 21:04:36 +0700

    Add Sample projects

--- SMBD/Tugas2/views.py
+++ SMBD/Tugas2/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+  fetched_data = karyawan.objects.all()[::1]
+  context = dict(serialized_data=fetched_data)
+  return render(request, 'index.html', context)
