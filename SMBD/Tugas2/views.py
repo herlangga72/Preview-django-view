c15896ad2a5c077a09908d974288dce6c58041b8
herlangga72 <herlangga72@gmail.com>
2020-10-27 15:03:30 +0700

    add comment

--- SMBD/Tugas2/views.py
+++ SMBD/Tugas2/views.py
@@ -10,14 +10,21 @@ def karyawan_all(request):
   context = dict(serialized_data=fetched_data,title=judul,column_name=kolom)
   return render(request, 'index.html', context)
 def product_counter(request):
+  #yg ini buat atur halaman
   judul = "Product Supply"
+  #yg ini buat judul kolom
   kolom = ["nama","total supply"]
+  #kita ambil semua data produknya
   product_stacks=produk.objects.all()
+  #kita buat sesuatu buat nampung data joinnya
   data = list()
-  #reconstuct data
+  #kita rekonstruksi datanya
   for product in product_stacks:
+    #kita ambil nama prioduknya dan agregasi agar keluar jumlah total produknya
     temp = mengemas.objects.filter(kode=product.kode).aggregate(Sum("jumlah"))['jumlah__sum']
+    #karena mereka hanya suport dictionary buat output jadi saya bikin juga
     data.append({'nama':product.nama,'jumlah':temp})
+  #terus kita masukin data yg didapet tadi ke kontextnya, kenapa nama nya serialize_data karena dat buakan dalam wujud object namun dict
   context = dict(serialized_data=data,title=judul,column_name=kolom)
   return render(request, 'index.html', context)
 
