0c31095654d203e7603a762d9c86681944b7b79c
herlangga72 <herlangga72@gmail.com>
2020-10-28 09:39:44 +0700

    Simplfy with annotate

--- SMBD/Tugas2/views.py
+++ SMBD/Tugas2/views.py
@@ -12,21 +12,9 @@ def karyawan_all(request):
   return render(request, 'index.html', context)
 
 def product_counter(request):
-  #yg ini buat atur halaman
   judul = "Product Supply"
-  #yg ini buat judul kolom
   kolom = ["nama","total supply"]
-  #kita ambil semua data produknya
-  product_stacks=produk.objects.all()
-  #kita buat sesuatu buat nampung data joinnya
-  data = list()
-  #kita rekonstruksi datanya
-  for product in product_stacks:
-    #kita ambil nama prioduknya dan agregasi agar keluar jumlah total produknya
-    temp = mengemas.objects.filter(kode=product.kode).aggregate(Sum("jumlah"))['jumlah__sum']
-    #karena mereka hanya suport dictionary buat output jadi saya bikin juga
-    data.append({'nama':product.nama,'jumlah':temp})
-  #terus kita masukin data yg didapet tadi ke kontextnya, kenapa nama nya serialize_data karena dat buakan dalam wujud object namun dict
+  data = mengemas.objects.values("kode__nama").annotate(jumlah=Sum("jumlah"))
   context = dict(serialized_data=data,title=judul,column_name=kolom)
   return render(request, 'index.html', context)
 
@@ -35,7 +23,6 @@ def karyawan_works_on(request):
   kolom = ["nama","list barang"]
   workers=karyawan.objects.all()
   data = list()
-  #reconstuct data
   for worker in workers:
     temp = mengemas.objects.filter(tgl_kemas=datetime.now().date()).filter(nomor=worker.nomer)
     data.append({'nama':worker.nama,'type':temp})
@@ -46,13 +33,7 @@ def karyawan_works_on(request):
 def Bonus_bulanan(request):
   judul = "Bonus Karyawan bulan ini"
   kolom = ["nama","bonus"]
-  #ambil data mengemas dan juga filter berdasar bulan ini
-  workers=karyawan.objects.all()
-  data = list()
-  #reconstuct data
-  for worker in workers:
-    bonus_one_month= mengemas.objects.filter(tgl_kemas__month=datetime.now().month).filter(nomor=worker.nomer).aggregate(Sum("bonus"))['bonus__sum']
-    data.append({'nama':worker.nama, 'jumlah':bonus_one_month})
+  data=mengemas.objects.values('nomor__nama').filter(tgl_kemas__month=datetime.now().month).annotate(jumlah=Sum("bonus"))
   context = dict(serialized_data=data,title=judul,column_name=kolom)
   return render(request, 'index.html', context)
 
